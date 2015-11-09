f0b3d5a2c16cfdfa9c67a63d7fba0a9ef6be1616
Christopher D. McMurrough <christopher.mcmurrough@gmail.com>
2015-11-08 18:28:07 -0600

    updated hello world examples

--- examples/hello_world.s
+++ examples/hello_world.s
@@ -1,28 +1,20 @@
 /******************************************************************************
 * @FILE hello_world.s
-* @BRIEF Hello world console print example
-*
-* Simple example of printing text to console using ARM assembly on Raspbian
-* Example based on "Raspberry Pi Assembly Language Raspbian" by Bruce Smith
-*
+* @BRIEF Simple hello world example using printf
 * @AUTHOR Christopher D. McMurrough
 ******************************************************************************/
-
-	.global  _start
-    
-_start:
-	MOV R7, #4          @ write syscall, 4
- 	MOV R0, #1          @ output stream to monitor, 1
-	MOV R2, #13         @ print string length
-	LDR R1,=hello_str   @ string at label hello_str:
-	SWI 0               @ execute syscall
-	B   _exit           @ branch to exit procedure
-    
+ 
+    .global main
+    .func main
+   
+main:
+    LDR R0,=print_str   @ store string address in R0
+    BL printf           @ call printf
+    B   _exit           @ branch to exit procedure with no return
+   
 _exit:   
 	MOV R7, #1          @ terminate syscall, 1
 	SWI 0               @ execute syscall
 
 .data
-hello_str:
-.ascii "Hello World!\n"
-
+print_str:  .asciz "Hello World!\n"
