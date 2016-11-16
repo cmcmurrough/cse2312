cb56f4811dab0922eec24dc303ef838a8025ce4a
Christopher D. McMurrough <cmcmurrough@users.noreply.github.com>
2016-11-16 16:38:53 -0600

    Update array_search.s

--- examples/array_search.s
+++ examples/array_search.s
@@ -16,7 +16,7 @@ writeloop:
     ADD R2, R1, R2          @ R2 now has the element address
     PUSH {R0}
     BL _rand                @ get a random number
-    STR R2, R0            @ write the address of a[i] to a[i]
+    STR R0, [R2]            @ write the address of a[i] to a[i]
     POP {R0}
     ADD R0, R0, #1          @ increment index
     B   writeloop           @ branch to next loop iteration
