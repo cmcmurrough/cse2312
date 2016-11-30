2a8da185d420f4b6e5d9d20df980df7333ee1929
Christopher D. McMurrough <christopher.mcmurrough@gmail.com>
2016-11-30 15:14:27 -0600

    added incomplete float_scanf example

---
+++ examples/float_scanf.s
@@ -0,0 +1,60 @@
+/******************************************************************************
+* @file float_scanf.s
+* @brief example of obtaining a floating point value using scanf
+*
+* Obtains a floating point value using scanf. The single precision number is
+* stored in memory, moved to R0, where it can then be moved to S0 for operation
+* in the FPU.
+*
+* @author Christopher D. McMurrough
+******************************************************************************/
+ 
+.global main
+.func main
+   
+main:
+    BL  _prompt             @ branch to prompt procedure with return
+    BL  _scanf              @ branch to scanf procedure with return
+    MOV R1, R0              @ move return value R0 to argument register R1
+    BL  _printf             @ branch to print procedure with return
+    B   _exit               @ branch to exit procedure with no return
+   
+_exit:  
+    MOV R7, #4              @ write syscall, 4
+    MOV R0, #1              @ output stream to monitor, 1
+    MOV R2, #21             @ print string length
+    LDR R1, =exit_str       @ string at label exit_str:
+    SWI 0                   @ execute syscall
+    MOV R7, #1              @ terminate syscall, 1
+    SWI 0                   @ execute syscall
+
+_prompt:
+    MOV R7, #4              @ write syscall, 4
+    MOV R0, #1              @ output stream to monitor, 1
+    MOV R2, #31             @ print string length
+    LDR R1, =prompt_str     @ string at label prompt_str:
+    SWI 0                   @ execute syscall
+    MOV PC, LR              @ return
+       
+_printf:
+    MOV R4, LR              @ store LR since printf call overwrites
+    LDR R0, =printf_str     @ R0 contains formatted string address
+    MOV R1, R1              @ R1 contains printf argument (redundant line)
+    BL printf               @ call printf
+    MOV PC, R4              @ return
+    
+_scanf:
+    PUSH {LR}               @ store LR since scanf call overwrites
+    SUB SP, SP, #4          @ make room on stack
+    LDR R0, =format_str     @ R0 contains address of format string
+    MOV R1, SP              @ move SP to R1 to store entry on stack
+    BL scanf                @ call scanf
+    LDR R0, [SP]            @ load value at SP into R0
+    ADD SP, SP, #4          @ restore the stack pointer
+    POP {PC}                @ return
+
+.data
+format_str:     .asciz      "%f"
+prompt_str:     .asciz      "Type a number and press enter: "
+printf_str:     .asciz      "The number entered was: %d\n"
+exit_str:       .ascii      "Terminating program.\n"
